62509ba041cca5fba825d167a4a1d4d6d157159e
liulinboyi <814921718@qq.com>
2020-07-21 03:01:46 +0800

    jsx普通标签等价代码

--- src/index.js
+++ src/index.js
@@ -17,3 +17,30 @@ let a = <div name="a">
 console.log(a)
 
 document.body.appendChild(a)
+
+
+
+/** 
+ * 
+ * 
+ * let a = <div name="a">
+  <span>hello</span>
+  <span>haha</span>
+  <span>huhu</span>
+</div>
+
+console.log(a)
+
+document.body.appendChild(a)
+
+
+ * 上面代码等价于
+ * var a = _toy_react__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("div", {
+  name: "a"
+}, 
+_toy_react__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("span", null, "hello"), _toy_react__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("span", null, "haha"), _toy_react__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("span", null, "huhu"));
+
+console.log(a);
+document.body.appendChild(a);
+ * 
+*/
